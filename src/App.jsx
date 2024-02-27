72adc27e288e41a8f5454bbab71cee89e339cb84
edvardsanta <eduardosantamarco123@outlook.com>
2024-02-27 18:37:10 -0300

    Add routes App.jsx
    - ProtectedRoute is a protected layer to access main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import FileUploadPage from "./pages/FileUploadPage";
-
+import RegistrationForm from "./pages/RegistrationForm";
+import LoginPage from "./pages/LoginPage";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
+  const handleLogin = (credentials) => {
+    console.log("Logic")
+  };
   return (
     <div className="d-flex flex-column align-items-center justify-content-center min-vh-100">
-      <FileUploadPage />
+      <Router>
+        <Routes>
+          <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <FileUploadPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>   
     </div>
   );
 }
