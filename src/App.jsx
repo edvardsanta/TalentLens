e28b35aec3d68d342ea93a376511a25a4eef8ca9
edvardsanta <eduardosantamarco123@outlook.com>
2024-04-26 15:59:51 -0300

    Change App.jsx
    - Removed login logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,29 @@ import FileUploadPage from "./pages/FileUploadPage";
 import RegistrationForm from "./pages/RegistrationForm";
 import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./routes/ProtectedRoute";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  const handleLogin = (credentials) => {
-    console.log("Logic")
-  };
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center min-vh-100">
-      <Router>
-        <Routes>
-          <Route path="/register" element={<RegistrationForm />} />
-          <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <FileUploadPage />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </Router>   
-    </div>
+      <div className="d-flex flex-column align-items-center justify-content-center min-vh-100">
+        <Router>
+          <Routes>
+            <Route path="/register" element={<RegistrationForm />} />
+            <Route
+              path="/login"
+              element={<LoginPage />}
+            />
+            <Route path="/" element={<ProtectedFileUploadPage />} />
+          </Routes>
+        </Router>
+      </div>
   );
 }
-
+const ProtectedFileUploadPage = () => {
+  return (
+    <ProtectedRoute>
+      <FileUploadPage />
+    </ProtectedRoute>
+  );
+};
 export default App;
