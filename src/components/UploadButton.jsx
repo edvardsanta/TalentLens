ddc1c7ee333736aeb3e4eef050b77a80bdc04840
EDU-COMPUTER\Eduardo <eduardosantamarco123@outlook.com>
2025-02-06 20:20:15 -0300

    Delete files

--- src/components/UploadButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const UploadButton = ({ onUpload }) => {
-  return (
-    <div className="d-grid gap-2 d-md-flex justify-content-md-center">
-      <button className="btn btn-primary" onClick={onUpload}>Upload</button>
-    </div>
-  );
-};
-export default UploadButton;
